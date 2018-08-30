999260fe20c88157777d417c41cf335679eadfa4
Marc Mills <mdmills1990@gmail.com>
2018-08-30 20:18:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@artist_count = {}
-  @@genre_count = {}
+
 
 attr_accessor :name, :artist, :genre
 
@@ -17,8 +16,6 @@ def initialize(name, artist, genre)
   @@count += 1
   @@artists << @artist
   @@genres << @genre
-  @@artist_count = {:@@artists => @@artists.count}
-  @@genre_count = {:@@genres => @@genres.count}
 end
 
 def self.count
@@ -35,7 +32,14 @@ end
 
 
 def self.artist_count
-  @@artist_count
+  artist_count = {}
+  
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
+    else
+      artist_count[artist] = 1
+    end
 end
 
 
