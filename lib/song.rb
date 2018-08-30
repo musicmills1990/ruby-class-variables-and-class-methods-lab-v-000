86a7fa404fda1db9396b9f573c68963b258cddb6
Marc Mills <mdmills1990@gmail.com>
2018-08-30 19:43:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ end
 
 
 def self.genre_count
-  @@genre_count.each do |genre, count|
+  @@genre_count.collect do |genre, count|
      genre => count
     #something to include each genre key and start with a count of 1, then if there are multiple "key => 1"s,
     #combine them together to make the count go to 2...
